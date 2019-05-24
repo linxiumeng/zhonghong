bc90729d860e5f9cf59c1933ba7e6a163a1364cf
nuoee <hb@nuoee.com>
2019-05-24 11:21:03 +0800

    add file get

--- blade-service/blade-infomation/src/main/java/org/springblade/information/controller/UploadController.java
+++ blade-service/blade-infomation/src/main/java/org/springblade/information/controller/UploadController.java
@@ -1,6 +1,8 @@
 package org.springblade.information.controller;
 
 import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.GetObjectRequest;
+import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.PutObjectResult;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.auth.sts.AssumeRoleRequest;
@@ -18,21 +20,23 @@ import org.springblade.common.config.CloudStorageConfig;
 import org.springblade.common.exception.RRException;
 import org.springblade.common.utils.R;
 import org.springblade.information.service.BannerService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("upload")
+@RequestMapping("file")
 @Api(tags = "文件上传（upload）")
 public class UploadController {
 
@@ -43,9 +47,6 @@ public class UploadController {
 
     private CloudStorageConfig config = new CloudStorageConfig();
     OSSClient ossClient = new OSSClient(config.getAliyunEndPoint(), config.getAliyunAccessKeyId(),
-            config.getAliyunAccessKeySecret());
-
-    OSSClient ossPrivateClient = new OSSClient(config.getAliyunEndPoint(), config.getAliyunAccessKeyId(),
             config.getAliyunAccessKeySecret());
 
     @PostMapping("/upload")
@@ -74,10 +75,21 @@ public class UploadController {
         return R.ok(url);
     }
 
-    @PostMapping("/privateFileUrl")
+    @GetMapping("/privateFileUrl/{fileName}")
     @Login
-    public R getStsInfo(String objectName) throws Exception {
+    public void getStsInfo(@PathVariable("fileName") String objectName, HttpServletResponse servletResponse) throws Exception {
 
+        OSSObject ossObject = ossClient.getObject(config.getAliyunNuoeePrivateBucketName(),objectName);
+        InputStream fileInputStream = ossObject.getObjectContent();
+        BufferedImage bufferedImage = ImageIO.read(fileInputStream);
+        ServletOutputStream out = servletResponse.getOutputStream();
+        ImageIO.write(bufferedImage,"png",out);
+        ossClient.shutdown();
+
+    }
+
+
+    private void getFileWithSts(String objectName){
         String endpoint = "sts.aliyuncs.com";
         String accessKeyId = "LTAIFeV8lu5U5sOU";
         String accessKeySecret = "ST7cYpABerW8dGjwngp0eSQSmaOxfG";
@@ -119,15 +131,23 @@ public class UploadController {
             logger.error("Failed：{}", ExceptionUtils.getFullStackTrace(e));
         }
 
-        OSSClient ossClient = new OSSClient(endpoint, response.getCredentials().getAccessKeyId(), response.getCredentials().getAccessKeySecret(), response.getCredentials().getSecurityToken());
+        String fileEndPoint = "http://oss-cn-hangzhou.aliyuncs.com";
+        OSSClient ossClient = new OSSClient(fileEndPoint, response.getCredentials().getAccessKeyId(), response.getCredentials().getAccessKeySecret(), response.getCredentials().getSecurityToken());
         //    OSSClient ossClient = new OSSClient(endpoint,"LTAI6C8UVSnZIV07","5KO3tKKh8UBhKoB9BQKahxfEHAgUo0");
         // 设置URL过期时间为1小时。
-        Date expiration = new Date(System.currentTimeMillis() + 3600 * 1000);
+        // Date expiration = new Date(System.currentTimeMillis() + 3600 * 1000);
         // 生成以GET方法访问的签名URL，访客可以直接通过浏览器访问相关内容。
-        URL url = ossClient.generatePresignedUrl("nuoee", objectName, expiration);
+        /*URL url = ossClient.generatePresignedUrl("nuoee", objectName, expiration);
         System.out.println(url.getQuery());
-        return R.ok().put("result",url.getQuery());
+        return R.ok().put("result",url.getQuery());*/
+        OSSObject ossObject = ossClient.getObject("nuoee",objectName);
+        InputStream fileInputStream = ossObject.getObjectContent();
+     //   BufferedImage bufferedImage = ImageIO.read(fileInputStream);
+     //   ServletOutputStream out = servletResponse.getOutputStream();
+     //   ImageIO.write(bufferedImage,"png",out);
+        ossClient.shutdown();
     }
+
 
 
 
