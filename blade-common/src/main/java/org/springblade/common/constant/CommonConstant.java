8b7473d130df213b41ca565fd68495f7919edff4
nuoee <hb@nuoee.com>
2019-05-30 16:25:34 +0800

    fix

--- blade-common/src/main/java/org/springblade/common/constant/CommonConstant.java
+++ blade-common/src/main/java/org/springblade/common/constant/CommonConstant.java
@@ -10,12 +10,12 @@ public interface CommonConstant {
 	/**
 	 * nacos dev 地址
 	 */
-	String NACOS_DEV_ADDR = "10.71.0.109";
+	String NACOS_DEV_ADDR = "localhost:8848";
 
 	/**
 	 * nacos prod 地址
 	 */
-	String NACOS_PROD_ADDR = "172.30.0.48:8848";
+	String NACOS_PROD_ADDR = "localhost:8848";
 
 	/**
 	 * sentinel dev 地址
